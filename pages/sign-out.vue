844d45930613e6c88d96fdd085a19210bc77693f
Fabiana Moreno <56853764+Niushae@users.noreply.github.com>
2025-06-25 20:38:18 -0400

    Trtr 17 (#58)
    * config: db:migrate command
    
    * feat: show profile and sign out option

---
+++ pages/sign-out.vue
@@ -0,0 +1,14 @@
+<script lang="ts" setup>
+const authStore = useAuthStore();
+
+onMounted(() => {
+  authStore.signOut();
+  navigateTo("/");
+});
+</script>
+
+<template>
+  <div class="container card bg-base-300 min-h-72 mx-auto mt-4 flex items-center justify-center gap-4">
+    <span v-if="authStore.loading" class="loading loading-spinner loading-xl" />
+  </div>
+</template>
