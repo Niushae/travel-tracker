85cc3a2eb89247e3cc8ab4f984c87db504a96370
Niushae <fabiana.moreno.fm@gmail.com>
2025-08-22 21:55:42 -0400

    feat: locations page

---
+++ pages/dashboard/index.vue
@@ -0,0 +1,35 @@
+<script lang="ts" setup>
+const { data, status } = await useFetch("/api/locations", {
+  lazy: true,
+});
+</script>
+
+<template>
+  <div class="px-12 py-8">
+    <h2 class="text-2xl">
+      Locations
+    </h2>
+    <div v-if="status === 'pending'">
+      <span class="loading loading-spinner loading-xl" />
+    </div>
+    <div v-else-if="data && data.length > 0" class="flex flex-wrap mt-4 gap-2">
+      <div
+        v-for="location in data"
+        :key="location.id"
+        class="card card-compact bg-base-300 min-h-40 w-72 p-4"
+      >
+        <h3 class="text-xl">
+          {{ location.name }}
+        </h3>
+        <p>{{ location.description }}</p>
+      </div>
+    </div>
+    <div v-else class="flex flex-col gap-2">
+      <p>Add a location to get started</p>
+      <NuxtLink to="/dashboard/add" class="btn btn-primary w-40 mt-4">
+        Add Location
+        <Icon name="tabler:tabler:circle-plus-filled" size="24" />
+      </NuxtLink>
+    </div>
+  </div>
+</template>
